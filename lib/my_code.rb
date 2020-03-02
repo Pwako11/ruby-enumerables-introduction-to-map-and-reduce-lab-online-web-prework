b5f5ca550fd375d8eb0da58f069457c0ac47cfae
Patrick A <akolo@hotmail.com>
2020-03-02 03:28:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -78,7 +78,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-    if !source_array[i] 
+    if source_array[i] 
       return source_array
     end
     i += 1
