d18ff380369a49283eb5378873951b1f1f894314
Patrick A <akolo@hotmail.com>
2020-03-02 03:59:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,15 +68,7 @@ def reduce_to_total(source_array, starting_point =0)
 end 
 
 def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.length do 
-    if source_array[i] 
-      return source_array
-    else
-      return true
-    end
-    i += 1
-  end 
+  return source_array.reduce{ |x, point| !!x && !!point } 
 end 
 
 def reduce_to_any_true(source_array)
