0f92a9a86a179e45ba01320e4efceac0c0832fe2
Patrick A <akolo@hotmail.com>
2020-03-02 04:00:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -72,12 +72,5 @@ def reduce_to_all_true(source_array)
 end 
 
 def reduce_to_any_true(source_array)
-  i = 0 
-  while i < source_array.length do 
-    if !source_array[i] 
-      return FALSE
-    end
-    i += 1
-  end 
-
+  return source_array.reduce{ |x, point| !!x || !!point }
 end
